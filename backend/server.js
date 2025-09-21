ad1c4909fc75ed870b4323f1f9b327c5837a2312
Harsh Vardhan Singh <131952048+harshvardhan119@users.noreply.github.com>
2025-09-21 21:35:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors(  origin: "https://tasya-1nig.vercel.app", // frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // if you send cookies or auth headers)); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,3 +23,4 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
